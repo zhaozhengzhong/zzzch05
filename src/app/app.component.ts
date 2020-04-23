a72b33ab9f5d9da57ce5dd30172280c44e071519
mxhsb <1010195505@qq.com>
2020-04-23 16:29:11 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormBuilder, AbstractControl, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+function userNameValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^a/)) {
+    return { invalidUser: true };
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loginapp';
+  myForm: FormGroup;
+  userName: AbstractControl;//输入用户名的控件
+  password: AbstractControl;//输入密码的控件
+  name$: Observable<string>;
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      'userName': ['aaa', Validators.compose([Validators.required, userNameValidator])],
+      'password': ['', Validators.compose([Validators.required, Validators.minLength(5)])]
+    });
+    this.userName = this.myForm.controls['userName'];
+    this.password = this.myForm.controls['password'];
+    this.name$ = this.userName.valueChanges;
+  }
+  onSubmit(value: any) {
+    console.log(value);
+  }
 }
